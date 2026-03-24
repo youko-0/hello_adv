b4cb0770b2b6cd50d2161e2c91cfe4bc5b8da9d0
xieruchao <xieruchao@corp.netease.com>
2026-03-24 16:21:52 +0800

    资源映射

--- plot/plot_forum_entry.js
+++ plot/plot_forum_entry.js
@@ -1,16 +1,16 @@
 // 入口剧情
-console.log('[LOAD] plot_forum_entry');
+console.log('[LOAD] plot_entry');
 
-/**
- * [资源预加载锚点] @KEEP @PRELOAD
- * 警告：请勿删除此注释块！引擎依靠此处的字符串进行静态资源预加载。
- * uiId: 'cawn43cd'
- * uiId: 'nw48gnat'
- * uiId: 'dbjp9oun'
- * uiId: '01ec9a548af54c2a86d86bd299d14003'
- * uiId: 'dacbc56e73a94b3daa268f1a0616d8e2'
- * uiId: '1af34c5623d74b82aa12ff9259621b91'
-*/
+// ==========================================
+// [资源预加载锚点] @KEEP @PRELOAD
+// 警告：请勿删除此注释块！引擎依靠此处的字符串进行静态资源预加载。
+// uiId: 'cawn43cd'
+// uiId: 'nw48gnat'
+// uiId: 'dbjp9oun'
+// uiId: '5a63b4fc0a7041f38685971fcecfd962'
+// uiId: '097c0cbdd8d44985b1b2933760da7c88'
+// uiId: '94d2303586e64ea396122dd9fa964f77'
+// ==========================================
 
 await ac.callUI({
   name: 'callUI_desktop',
