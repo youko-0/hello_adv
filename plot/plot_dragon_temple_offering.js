3a8c4a5bcfdba8cdd6282f47037c43b2231fcac1
xieruchao <xieruchao@corp.netease.com>
2026-06-18 14:12:00 +0800

    道具使用

---
+++ plot/plot_dragon_temple_offering.js
@@ -0,0 +1,147 @@
+// 龙三太子神像上贡剧情
+console.log('[LOAD] plot_dragon_temple_offering');
+
+const choiceIndex = await CommonUI.showCustomOptionGroup({
+    options: [
+        { content: '不上贡' },
+        { content: '上贡' },
+    ],
+});
+
+let itemId = '';
+if (choiceIndex === 1) {
+    itemId = await BagUI.open({ mode: 'choose' });
+    console.log('[LOG] 选择物品', itemId);
+}
+
+switch (itemId) {
+    case 'item_spirit_eye':
+        await ac.sysDialogOn({
+            roleName: '？？',
+            content: '是李云祥赐予你的技能？那你就好好收着吧。',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        break;
+
+    case 'item_pendant':
+        await ac.sysDialogOn({
+            roleName: '？？',
+            content: '唔……是我的龙鳞，上面似乎刻着模糊的字迹，但还是把它留给李云祥吧。',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        break;
+
+    case 'item_armor':
+        await ac.sysDialogOn({
+            roleName: '？？',
+            content: '拿走，这辈子没穿过这么短的衣服。',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        break;
+
+    case 'item_compass':
+        await ac.sysDialogOn({
+            roleName: '？？',
+            content: '这里面是李云祥前世的记忆啊，有意思……不过，我更想听他亲口告诉我。',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        break;
+
+    case 'item_visa':
+        await ac.sysDialogOn({
+            roleName: '？？',
+            content: '偷了我daddy的东西，还敢拿到本少爷面前？？你是活腻了吗？',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        break;
+
+    case 'item_motorcycle_key':
+        await ac.sysDialogOn({
+            roleName: '？？',
+            content: '（开心地哼歌）哼哼，李云祥那家伙居然敢不给本少爷红莲，还不是叫我拿到了~',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        await ac.sysDialogOn({
+            roleName: '我',
+            content: '唔……我好像见过你。',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        await ac.sysDialogOn({
+            roleName: '我',
+            content: '你该不会是那个……',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        await ac.sysDialogOn({
+            roleName: '？？',
+            content: '好没新意的杀猪盘套路。',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        await ac.sysDialogOn({
+            roleName: '我',
+            content: '（怒）才不是杀猪盘！',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        await ac.sysDialogOn({
+            roleName: '？？',
+            content: '咦？仔细一看，你确实让我有点眼熟……',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        await ac.sysDialogOn({
+            roleName: '？？',
+            content: '看在你诚心诚意向我献上红莲钥匙的份上，本太子就大慈大悲地赏你一件礼物吧。',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        await ac.sysDialogOn({
+            roleName: '我',
+            content: '（立刻跪下，伸出双手）谢谢丙丙大王，大王万岁万岁万万岁。',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        await ac.sysDialogOn({
+            roleName: '？？',
+            content: '（嫌弃）居然咒我短命。',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        await InventoryUI.gainItem('item_blessing')
+        break;
+
+    default:
+        // 未选择物品 或 直接选择不上贡
+        await ac.sysDialogOn({
+            roleName: '我',
+            content: '这次来得仓促，身上什么东西都没带，我看看比脸还干净的衣兜，只能放弃了孝敬小舅妈的机会。\n下次再来吧，下次一定！',
+            hasRoleName: true,
+            hasBg: true,
+            hasRoleAvatar: false,
+        });
+        break;
+}
+
+await ac.sysDialogOff({});
